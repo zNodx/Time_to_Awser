a5e5fe2e5bd39dfb69cd7ef8616f3ef440bc80b4
mateus <ffrmateus@gmail.com>
2022-03-08 15:58:58 -0300

    Criando uma 'rake task' para os usuários padrão

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :users
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+  devise_for :admins
+  get 'home', to: 'site/welcome#index'
+
+  root to: 'site/welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
