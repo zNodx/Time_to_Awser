7d55eba4e02e9740f8469333fb3a4bbb2fffe387
mateus <ffrmateus@gmail.com>
2022-03-18 11:09:48 -0300

    Layout update - Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
+  
   layout :layout_by_resource
-protected
-  def layout_by_resource
-    if devise_controller? && resource_class == Admin
-    "admin_devise"
-    else
-    "application"
+
+  protected
+
+    def layout_by_resource
+    devise_controller? ?  "#{resource_class.to_s.downcase}_devise" : "application"
     end
-  end
 end
