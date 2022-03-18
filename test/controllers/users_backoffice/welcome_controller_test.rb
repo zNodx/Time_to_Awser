d24ac1e261ce8ee798da74792285be4de5b6323d
mateus <ffrmateus@gmail.com>
2022-03-18 09:24:30 -0300

    Cocoon Config

--- test/controllers/users_backoffice/welcome_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UsersBackoffice::WelcomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get users_backoffice_welcome_index_url
-    assert_response :success
-  end
-
-end
