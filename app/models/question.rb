d1aed3fc3abda252ca0020ae656de30e586ef1a1
mateus <ffrmateus@gmail.com>
2022-03-18 15:23:41 -0300

    Remove register of admin

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,22 @@
 class Question < ApplicationRecord
-  belongs_to :subject, inverse_of: :questions
+  belongs_to :subject, counter_cache: true, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
   
     scope :_search_, ->(page, term) {
-      includes(:answers)
+      includes(:answers, :subject)
       .where("lower(description) LIKE ?", "%#{term.downcase}%")
       .page(page)
     }
  
     scope :last_questions, ->(page) {
-      includes(:answers).order('created_at desc').page(page)
+      includes(:answers, :subject).order('created_at desc').page(page)
+    }
+
+    scope :_search_subject_, ->(page, subject_id){
+      includes(:answers, :subject)
+      .where(subject_id: subject_id)
+      .page(page)
     }
 end
