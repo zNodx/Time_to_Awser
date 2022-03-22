c4fd473b8c874ba9a2a45d60f9d66030cb89a8f4
mateus <ffrmateus@gmail.com>
2022-03-22 10:10:13 -0300

    Nova branch

--- app/assets/javascripts/users_backoffice.coffee
+++ app/assets/javascripts/users_backoffice.coffee
@@ -5,4 +5,7 @@
 //= require bootstrap/dist/js/bootstrap
 //= require fastclick/lib/fastclick
 //= require nprogress/nprogress
+//= require bootstrap-growl-ifightcrime/jquery.bootstrap-growl
+//= require jquery-ui/ui/widgets/datepicker
+//= require jquery-ui/ui/i18n/datepicker-pt-BR
 //= require custom
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  validates :first_name, presence: true, length: { minimum:2 }, on: :update
+  devise :database_authenticatable, :registerable,:recoverable, :rememberable, :validatable
+  has_one :user_profile
+  accepts_nested_attributes_for :user_profile, reject_if: :all_blank
+  def full_name
+    [self.first_name, self.last_name].join(' ')
+  end
+  
 end
