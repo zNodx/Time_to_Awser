087a4b65aa3cbb5ccb9276c0042b89c5c389d3c4
mateus <ffrmateus@gmail.com>
2022-03-23 15:14:42 -0300

    Project - Finished

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,7 +1,8 @@
 class Site::SearchController < SiteController 
     def questions
-        @questions = Question._search_(params[:page], params[:term])
-        end
+        Question.reindex
+        @questions = Question.search(params[:term], page: params[:page], per_page: 5)        
+    end
     def subject
         @questions = Question._search_subject_(params[:page],
         params[:subject_id])
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,17 @@
 class Question < ApplicationRecord
+  searchkick
+  
   belongs_to :subject, counter_cache: true, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
 
+  # Callback
   after_create :set_statistic
 
+  # Kaminari
   paginates_per 5
 
+  # Scopes
   scope :_search_subject_, ->(page, subject_id){
     includes(:answers, :subject)
     .where(subject_id: subject_id)
